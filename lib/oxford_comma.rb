06b5ac701a2532dfe0b4f5f2156a6fd1ee73f0e6
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-07 00:06:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   new_array=[]
   if array.size == 1
     return array.join
+  elsif array.size == 2
+    return array.join(" and ")
   else
     array.each_with_index do |element, index|
       if index == array.size - 1
