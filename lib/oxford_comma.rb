ea6db1d7f2368ae8c045900ffeebb1a3f79979f6
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-07 00:05:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,13 @@
 def oxford_comma(array)
+  new_array=[]
   if array.size == 1
     return array.join
+  else
+    array.each_with_index do |element, index|
+      if index == array.size - 1
+        new_array.push(element.prepend("and "))
+      end
+    end
   end
 
 end
